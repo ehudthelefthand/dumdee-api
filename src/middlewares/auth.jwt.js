4915d40ebcdef204bca22fe806617adeecf95891
Peerawat Poombua <peerawat@odds.team>
2020-09-16 03:10:16 +0700

    update middleware code

--- src/middlewares/auth.jwt.js
+++ src/middlewares/auth.jwt.js
@@ -2,33 +2,15 @@ const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 
 const setUser = (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        return next()
-    }
-    try {
-        const decoded = jwt.verify(token, SECRET);
-        req.user = decoded;
-        return next()
-    } catch (err) {
-        return res.sendStatus(401)
-    }
+    next()
 }
 
 const requireUser = (req, res, next) => {
-    if (req.user) {
-        return next()
-    }
-    return res.sendStatus(401)
+    next()
 }
 
 const requireAdmin = (req, res, next) => {
-    // This is just an illustrate
-    // Don't use in real production
-    if (req.user && req.user.email === 'admin@email.com') {
-        return next()
-    }
-    return res.sendStatus(401)
+    next()
 }
 
 module.exports = {
